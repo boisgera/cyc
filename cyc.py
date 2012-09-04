db72889fcceb6277b89bd4699d6a2a18911c9132
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2012-09-04 14:33:58 +0200

    works as a script

--- cyc.py
+++ cyc.py
@@ -1,25 +1,49 @@
 #!/usr/bin/env python
 
+"""
+Cython-based compiler
+"""
+
 # Python 2.7 Standard Library
+import contextlib
+import os.path
 import sys
+import shutil
+import tempfile
 
 # Third-Party Libraries
 import pbs
 
-# -----
 
 PYTHON_CFLAGS = str(pbs.pkg_config("python", cflags=True)).strip()
 
-def compile(filename):
+join = os.path.join
+
+@contextlib.contextmanager
+def temp_dir():
+    dir = tempfile.mkdtemp()
+    try:
+        yield dir
+    finally:
+        shutil.rmtree(dir)
+
+def compile(filename, build_dir=""):
     """gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing \
       $(CFLAGS) -o mod.so mod.c
     """
     basename = ".".join(filename.split(".")[:-1])
-    pbs.cython(filename, o=basename + ".c")
+    cname = join(build_dir, basename + ".c")
+    pbs.cython(filename, o=cname)
     args = "-shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing".split()
     args.append([PYTHON_CFLAGS])
-    args.append(["-o", basename + ".so", basename + ".c"])
+    args.append(["-o", basename + ".so", cname])
     pbs.gcc(*args)
+
+def main(filenames):
+    with temp_dir() as build_dir:
+        for filename in filenames:
+            compile(filename, build_dir)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
+
